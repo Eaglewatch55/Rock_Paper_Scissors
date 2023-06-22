0a7007344155dc82b9b4e38646080bcd38f3bdf5
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-06-22 08:56:32 -0600

    Stage 1

---
+++ Rock-Paper-Scissors (Python)/task/game.py
@@ -0,0 +1,6 @@
+# Write your code here
+user_input = input()
+
+lose_dict = {"rock": "paper", "paper": "scissors ", "scissors": "rock"}
+
+print(f"Sorry, but the computer chose {lose_dict[user_input]}")
