b29306c2157e277611d17e971f27331539f2f7d8
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-06-22 09:15:51 -0600

    Stage 2

--- Rock-Paper-Scissors (Python)/task/game.py
+++ Rock-Paper-Scissors (Python)/task/game.py
@@ -1,6 +1,15 @@
 # Write your code here
+import random
+#random.seed(10)
 user_input = input()
 
-lose_dict = {"rock": "paper", "paper": "scissors ", "scissors": "rock"}
+lose_dict = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+options = ["rock", "paper", "scissors"]
+comp_choice = random.choice(options)
 
-print(f"Sorry, but the computer chose {lose_dict[user_input]}")
+if lose_dict[user_input] == comp_choice:
+    print(f"Sorry, but the computer chose {lose_dict[user_input]}")
+elif user_input == comp_choice:
+    print(f"There is a draw {user_input}")
+elif lose_dict[comp_choice] == user_input:
+    print(f"Well done. The computer chose {comp_choice} and failed")
