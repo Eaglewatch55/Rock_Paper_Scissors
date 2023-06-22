c2f644ecb5515f461d5ff13a258d46ae9c7e5386
Eaglewatch55 <orlandogarcia94@hotmail.com>
2023-06-22 09:20:24 -0600

    Stage 3

--- Rock-Paper-Scissors (Python)/task/game.py
+++ Rock-Paper-Scissors (Python)/task/game.py
@@ -1,15 +1,23 @@
 # Write your code here
 import random
 #random.seed(10)
-user_input = input()
 
-lose_dict = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
-options = ["rock", "paper", "scissors"]
-comp_choice = random.choice(options)
+while True:
+    user_input = input()
 
-if lose_dict[user_input] == comp_choice:
-    print(f"Sorry, but the computer chose {lose_dict[user_input]}")
-elif user_input == comp_choice:
-    print(f"There is a draw {user_input}")
-elif lose_dict[comp_choice] == user_input:
-    print(f"Well done. The computer chose {comp_choice} and failed")
+    if user_input == "!exit":
+        print("Bye!")
+        break
+
+    lose_dict = {"rock": "paper", "paper": "scissors", "scissors": "rock"}
+    options = ["rock", "paper", "scissors"]
+    comp_choice = random.choice(options)
+
+    if lose_dict.get(user_input) == comp_choice:
+        print(f"Sorry, but the computer chose {lose_dict[user_input]}")
+    elif user_input == comp_choice:
+        print(f"There is a draw {user_input}")
+    elif lose_dict.get(comp_choice) == user_input:
+        print(f"Well done. The computer chose {comp_choice} and failed")
+    else:
+        print("Invalid input")
